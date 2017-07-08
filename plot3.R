b7cd2a97150143abfc72184b899c36ff1486396a
VanZeidt <elekes.peter.g@gmail.com>
2017-07-08 19:09:08 +0200

    deleted unnecessary comment

--- plot3.R
+++ plot3.R
@@ -1,5 +1,4 @@
 # Peer-graded Assignment: Course Project 1
-# Plot 1
 
 # Download data
 download.file(url = 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', 
